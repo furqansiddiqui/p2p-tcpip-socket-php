ccee15aa7b8fb1dc6e3ab733a788bd695511e9fe
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-03-05 17:22:00 +0500

    communication exceptions

---
+++ src/Exception/PeerCommunicateException.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This file is a part of "furqansiddiqui/p2p-tcpip-socket-php" package.
+ * https://github.com/furqansiddiqui/p2p-tcpip-socket-php
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/furqansiddiqui/p2p-tcpip-socket-php/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace FurqanSiddiqui\P2PSocket\Exception;
+
+use FurqanSiddiqui\P2PSocket\Peers\Peer;
+use Throwable;
+
+/**
+ * Class PeerCommunicateException
+ * @package FurqanSiddiqui\P2PSocket\Exception
+ */
+class PeerCommunicateException extends P2PSocketException
+{
+    /** @var Peer */
+    private $peer;
+
+    /**
+     * PeerCommunicateException constructor.
+     * @param Peer $peer
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct(Peer $peer, $message = "", $code = 0, Throwable $previous = null)
+    {
+        $this->peer = $peer;
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * @return Peer
+     */
+    public function peer(): Peer
+    {
+        return $this->peer;
+    }
+}
+
---
+++ src/Exception/PeerWriteException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is a part of "furqansiddiqui/p2p-tcpip-socket-php" package.
+ * https://github.com/furqansiddiqui/p2p-tcpip-socket-php
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/furqansiddiqui/p2p-tcpip-socket-php/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace FurqanSiddiqui\P2PSocket\Exception;
+
+/**
+ * Class PeerWriteException
+ * @package FurqanSiddiqui\P2PSocket\Exception
+ */
+class PeerWriteException extends PeerCommunicateException
+{
+}
